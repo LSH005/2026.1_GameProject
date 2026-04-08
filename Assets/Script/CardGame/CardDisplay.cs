065e5812250d096e9fe8dd5a3edd8533826cf5f1
LSH005 <isihyeon005shingu@gmail.com>
2026-04-08 11:47:44 +0900

    [feat] 카드 적용 구현

--- Assets/Script/CardGame/CardDisplay.cs
+++ Assets/Script/CardGame/CardDisplay.cs
@@ -5,6 +5,7 @@ public class CardDisplay : MonoBehaviour
 {
 
     public CardData cardData;
+    public int cardIndex;
 
     public MeshRenderer cardRenderer;
     public TextMeshPro nameText;
@@ -12,8 +13,17 @@ public class CardDisplay : MonoBehaviour
     public TextMeshPro attackText;
     public TextMeshPro descriptionText;
 
+    bool isDragging = false;
+    Vector3 originalPosition;
+
+    public LayerMask enemyLayer;
+    public LayerMask playerLayer;
+
     private void Start()
     {
+        playerLayer = LayerMask.GetMask("Player");
+        enemyLayer = LayerMask.GetMask("Enemy");
+
         SetupCard(cardData);
     }
 
@@ -30,6 +40,80 @@ public class CardDisplay : MonoBehaviour
         {
             Material cardMaterial = cardRenderer.material;
             cardMaterial.mainTexture = data.artwork.texture;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        isDragging = true;
+        originalPosition = transform.position;
+    }
+
+    private void OnMouseDrag()
+    {
+        if (isDragging)
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = Camera.main.WorldToScreenPoint(transform.position).z;
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+            transform.position = new Vector3(worldPos.x, worldPos.y, transform.position.z);
+
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isDragging = false;
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        bool cardUsed = false;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, enemyLayer))
+        {
+            // 적에게 공격 효과 적용
+            CharacterStats enemyStats = hit.collider.GetComponent<CharacterStats>();
+
+            if (enemyStats != null)
+            {
+                if (cardData.cardType == CardData.CardType.Attack) // 카드 효과에 따라 다르게
+                {
+                    // 공격 카드면 데미지 추가
+                    enemyStats.TakeDamage(cardData.effectAmount);
+                    Debug.Log($"\"{cardData.cardName}\" 카드로 적에게 {cardData.effectAmount} 데미지를 입혔습니다.");
+                    cardUsed = true;
+                }
+                else
+                {
+                    Debug.Log("이 카드는 적에게 사용할 수 없습니다.");
+                }
+            }
+        }
+        else if (Physics.Raycast(ray, out hit, Mathf.Infinity, playerLayer))
+        {
+            CharacterStats playerStats = hit.collider.GetComponent<CharacterStats>();
+
+            if (playerStats != null)
+            {
+                if (cardData.cardType == CardData.CardType.Heal)
+                {
+                    playerStats.Heal(cardData.effectAmount);
+                    Debug.Log($"{cardData.cardName} 카드로 플레이어의 체력을 {cardData.effectAmount} 회복했습니다.");
+                    cardUsed = true;
+                }
+                else
+                {
+                    Debug.Log("이 카드는 플레이어에게 사용할 수 없습니다.");
+                }
+            }
+        }
+
+        if (!cardUsed)
+        {
+            transform.position = originalPosition;
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
 }
---
+++ Assets/Script/CardGame/CharacterStats.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CharacterStats : MonoBehaviour
+{
+    public string characterName;
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    // UI 요소
+    public Slider healthBar;
+    public TextMeshProUGUI healthText;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+    }
+}
